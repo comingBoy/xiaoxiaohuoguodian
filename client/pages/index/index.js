e68ee26af66238411576ed5f318ac7c52a69b52c
LiLinFeng <594244263@qq.com>
2018-05-01 21:36:38 +0800

    1

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1163,6 +1163,16 @@ Page({
       orderDateIndex: e.detail.value
     })
   },
+  /**
+   * 页面跳转
+   */
+  toPages: function (e) { 
+    console.log(e)
+    var page = e.currentTarget.dataset.page
+    wx.navigateTo({
+      url: page,
+    })
+  }
 })
 
 
